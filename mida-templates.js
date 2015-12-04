e98ac780b0cf708b9b14bdaab40da32f954a6f01
mida-templates <info.midatemplates@gmail.com>
2015-12-04 12:50:18 +0800

    Create mida-templates.js

---
+++ mida-templates.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "http://www.midatemplates.com/") {
+        window.location.href = "http://www.midatemplates.com"; }});
+//]]>
+</script>
